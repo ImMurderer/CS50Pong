7907ae3d02c6ef93eb8d9d0048d476aedf284743
ImMurderer <joaopedro0128@hotmail.com>
2019-04-19 04:40:57 -0300

    feat: paddles and font added

--- main.lua
+++ main.lua
@@ -9,6 +9,10 @@ VIRTUAL_HEIGHT = 243
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -25,12 +29,20 @@ end
 function love.draw()
     push:apply('start')
 
+    love.graphics.clear(.025, .02, .01, 1)
+
     love.graphics.printf(
         'Hello Pong!',
         0,
-        VIRTUAL_HEIGHT / 2 - 6,
+        20,
         VIRTUAL_WIDTH,
         'center')
 
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    
     push:apply('end')
 end
